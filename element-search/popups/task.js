f402f51eb6971016e4de2c513ed5f1d67756980e
Стефания <stt.orlova@gmail.com>
2022-03-30 10:27:11 +0300

    update task 1

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -2,17 +2,17 @@ const modalMain = document.getElementById("modal_main");
 const modalSuccess = document.getElementById("modal_success");
 modalMain.classList.add('modal_active');
 
-const modalMainClose = modalMain.querySelector('.modal__close');
-modalMainClose.addEventListener('click', () => modalMain.remove('modal_active'));
+const modalMainClose = document.querySelectorAll('.modal__close_times');
+Array.from(modalMainClose);
+
+for (let i = 0; i < modalMainClose.length; i++) {
+   modalMainClose[i].onclick = function () {
+      this.closest('.modal').classList.remove('modal_active');
+   };
+}
 
 const modalMainShow = modalMain.querySelector('.show-success');
 modalMainShow.addEventListener('click', () => {
-   modalMain.remove('modal_active');
-   modalSuccess.classList.add('modal_active');
+  modalMain.remove('modal_active');
+  modalSuccess.classList.add('modal_active');
 });
-
-const modalSuccessClose = modalSuccess.querySelector('.modal__close');
-modalSuccessClose.addEventListener('click', () => modalSuccess.remove('modal_active'));
-
-const modalSuccessShow = modalSuccess.querySelector('.btn_success');
-modalSuccessShow.addEventListener('click', () => modalSuccess.remove('modal_active'));
