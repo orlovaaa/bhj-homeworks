7409bc95053ad3da44b328ab19f9e311fb20a11d
Стефания <stt.orlova@gmail.com>
2022-04-11 19:48:14 +0300

    task 3

---
+++ dom/book-reader/task.js
@@ -0,0 +1,22 @@
+const book = document.getElementById('book');
+const font = Array.from(document.querySelectorAll('.font-size'));
+
+for (let i = 0; i < font.length; i++) {
+   font[i].addEventListener('click', (e) => {
+      e.preventDefault();
+      for (let i = 0; i < font.length; i++) {
+         if (font[i].classList.contains('font-size__active')) {
+            font[i].classList.toggle('font-size__active');
+         }
+      }
+
+      font[i].classList.toggle('font-size__active');
+      if (font[i].classList.contains('font-size_small')) {
+         book.className = "book book_fs-small";
+      } else if (font[i].classList.contains('font-size_big')) {
+         book.className = "book book_fs-big";
+      } else {
+         book.className = "book";
+      }
+   });
+}
