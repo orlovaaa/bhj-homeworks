28e4e7381a57a3d3c2004190f35c898a51a28513
Стефания <stt.orlova@gmail.com>
2022-04-20 14:32:31 +0300

    fix task 1

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -8,11 +8,16 @@ for (let i = 0; i < tooltip.length; i++) {
    tooltip[i].addEventListener('click', (e) => {
       e.preventDefault();
 
+      const left = tooltip[i].getBoundingClientRect().left;
+      const top = tooltip[i].getBoundingClientRect().bottom;
+
       if (div.textContent === tooltip[i].getAttribute('title')) {
          div.className = 'tooltip';
       } else {
          div.className = " tooltip tooltip_active";
       }
       div.textContent = `${tooltip[i].getAttribute('title')}`;
+      div.style.left = left + "px";
+      div.style.top = top + "px";
    });
 }
